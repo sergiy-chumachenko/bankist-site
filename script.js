0c52c7e84c1dd840b59880e4600718bcad086884
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-08-07 16:54:38 +0300

    manu fade animation has implemented

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const navigation = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -85,3 +86,22 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu Fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+// Passing args into handler
+navigation.addEventListener('mouseover', handleHover.bind(0.5));
+navigation.addEventListener('mouseout', handleHover.bind(1));
